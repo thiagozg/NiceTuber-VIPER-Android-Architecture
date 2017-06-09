312c1eb1a1585261e33df715990e83c40b09c632
thiagozg <thiagozg1995@gmail.com>
2017-06-09 00:18:36 -0300

    configuring gradle

---
+++ app/src/main/java/br/com/nicetuber/util/Constants.java
@@ -0,0 +1,11 @@
+package br.com.nicetuber.util;
+
+/**
+ * Developed by.:   @thiagozg on 08/06/2017.
+ * E-mail.:         thiagozg1995@gmail.com
+ * GitHub.:         https://github.com/thiagozg/
+ * Google Play.:    https://play.google.com/store/apps/developer?id=Thiago+Giacomini
+ */
+
+public class Constants {
+}
